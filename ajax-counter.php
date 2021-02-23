804411b0069a91e460b6b9bdc28da826ab0b862b
Manuel Badzong <manuel@andev.ch>
2021-02-23 11:45:19 +0000

    Allow view counting with different delays

--- ajax-counter.php
+++ ajax-counter.php
@@ -12,13 +12,24 @@
 add_action('wp_footer', function () {
 	if ( is_single(get_the_ID()) ): ?>
    	<script>
+		function ajax_counter_post(delay = 0) {
+			var action = 'ajax_counter'
+			var url = "<?php echo admin_url('admin-ajax.php'); ?>";
+			var post_id = "<?php echo get_the_ID(); ?>";
+			var nonce = "<?php echo wp_create_nonce(); ?>";
+
+			var data = { 'action': action, 'post_type': 'POST', 'post_id': post_id, 'delay': delay, '_ajax_nonce': nonce };
+
+			if ( delay > 0) {
+				window.setTimeout(function() { jQuery.post( url, data ); }, delay * 1000);
+			} else {
+				jQuery.post( url, data );
+			}
+		}
+
 		document.addEventListener('DOMContentLoaded', function(){ 
-			 window.setTimeout(function() {
-				jQuery.post(
-					"<?php echo admin_url('admin-ajax.php'); ?>",
-					{ 'action': 'ajax_counter', 'post_type': 'POST', 'post_id': '<?php echo get_the_ID(); ?>', '_ajax_nonce': '<?php echo wp_create_nonce(); ?>' }
-				);
-			}, 5000);
+			ajax_counter_post();
+			ajax_counter_post(10);
 		}, false);
 	</script>
 <?php endif; });
@@ -32,24 +43,55 @@ function ajax_counter() {
 	$meta_key = 'ajax_counter_views';
 
 	if ( !check_ajax_referer() || !array_key_exists('post_id', $_POST) ) {
-		echo 'ERROR';
+		echo 'ERROR: bad request';
                 exit;
         }
 
-	// SECURITY: Make sure post_id is in fact an id.
+	// SECURITY: Make sure post_id is numeric
         $post_id = trim($_POST['post_id']);
         if ( !preg_match('/^[0-9]+$/', $post_id) ) {
-		echo 'ERROR';
+		echo 'ERROR: id=$post_id';
                 exit;
         }
+
+	$delay = array_key_exists('delay', $_POST)? intval($_POST['delay']): 0;
+	if ($delay != 0 && $delay != 10) {
+		echo "ERROR: delay=$delay";
+                exit;
+	}
+
+	if ( $delay ) {
+		$meta_key .= '_' . $delay;
+	}
 
 	$meta_created = add_post_meta( $post_id, $meta_key, 1, true );
 	if ( !$meta_created ) {
     		$wpdb->query("UPDATE `$postmeta` SET meta_value = meta_value + 1 WHERE post_id = '$post_id' and meta_key = '$meta_key' ");
 	}
 	
-	echo 'OK';
+	echo "OK";
         exit;
 };
+
+function ajax_counter_views($delay = 0) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'postmeta';
+
+        $post_id = get_the_ID();
+        if ( !$post_id ) {
+                return false;
+        }
+
+	$meta_key = 'ajax_counter_views';
+	if ( $delay > 0 ) {
+		$meta_key .= '_' . $delay;
+	}
+	
+        // Use a database query to avoid caching problems
+        $query = "SELECT meta_value FROM `$table_name` WHERE meta_key='$meta_key' AND post_id='$post_id'";
+        $views = $wpdb->get_var($query);
+
+        return $views ? intval($views): 0;
+}
 
 ?>
